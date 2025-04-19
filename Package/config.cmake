5551b57dfc7782c5e4a7eb975b9d50c848780142
BrandonFoster <Brandon.Foster4525@outlook.com>
2025-04-19 15:22:58 -0400

    fixed cross-compile issues

--- Package/config.cmake
+++ Package/config.cmake
@@ -1,13 +1,15 @@
-add_custom_target(
-    GLEW-DLL-Copy
-    DEPENDS "${CMAKE_CURRENT_LIST_DIR}/__run_always"
-    COMMENT "GLEW DLL Copy"
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_CURRENT_LIST_DIR}/bin/glew.dll" "${CMAKE_BINARY_DIR}/$<CONFIG>/bin/glew.dll"
-)
- 
 add_library(glew-all INTERFACE)
-add_dependencies(glew-all GLEW-DLL-Copy)
 target_include_directories(glew-all INTERFACE "${CMAKE_CURRENT_LIST_DIR}/include")
 target_link_directories(glew-all INTERFACE "${CMAKE_CURRENT_LIST_DIR}/lib")
 target_link_libraries(glew-all INTERFACE glew)
 add_library(glew::all ALIAS glew-all)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    add_custom_target(
+        GLEW-DLL-Copy
+        DEPENDS "${CMAKE_CURRENT_LIST_DIR}/__run_always"
+        COMMENT "GLEW DLL Copy"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_CURRENT_LIST_DIR}/bin/glew.dll" "${CMAKE_BINARY_DIR}/$<CONFIG>/bin/glew.dll"
+    )
+    add_dependencies(glew-all GLEW-DLL-Copy)
+endif()
